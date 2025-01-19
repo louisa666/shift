016102b2b10bd69914709f06fea550d44cfef31b
Бахриева Луиза Равшановна <BakhrievaLR@gnivc.ru>
2025-01-19 21:25:48 +0700

    доработаны тестовые методы

--- src/test/java/autotests/DuckActionsTest.java
+++ src/test/java/autotests/DuckActionsTest.java
@@ -1,11 +1,8 @@
 package autotests;
 
 import com.consol.citrus.annotations.CitrusTest;
-import com.consol.citrus.model.testcase.core.ReceiveModel;
 import com.consol.citrus.testng.spring.TestNGCitrusSpringSupport;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.jayway.jsonpath.JsonPath;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import com.consol.citrus.TestCaseRunner;
@@ -16,7 +13,6 @@ import utils.DuckActionsUtils;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
-import static com.consol.citrus.http.actions.HttpActionBuilder.http;
 
 
 public class DuckActionsTest extends TestNGCitrusSpringSupport {
@@ -24,12 +20,12 @@ public class DuckActionsTest extends TestNGCitrusSpringSupport {
     private String host;
     String body;
     String responseBody;
+
     @BeforeTest
-    public void setup()
-    {
+    public void setup() {
         duckActions = new DuckActionsUtils();
         host = "http://localhost:2222";
-        body =  "{" +
+        body = "{" +
                 "  \"color\": \"yellow\"," +
                 "  \"height\": 5," +
                 "  \"material\": \"rubber\"," +
@@ -45,37 +41,29 @@ public class DuckActionsTest extends TestNGCitrusSpringSupport {
                 "  \"wingsState\": \"ACTIVE\"" +
                 "}";
     }
-    @Test(description = "Метод создания утки")
+
+    @Test(description = "Метод создания утки (material = rubber, material = wood")
     @CitrusTest
-    public void create (@Optional @CitrusResource TestCaseRunner runner) throws JsonProcessingException
-    {
-        ObjectMapper object  = new ObjectMapper();
+    public void create(@Optional @CitrusResource TestCaseRunner runner) throws JsonProcessingException {
+        ObjectMapper object = new ObjectMapper();
         JsonNode bodyJson = object.readTree(body);
         JsonNode bodyResponseJson = object.readTree(responseBody);
 
         duckActions.create(runner, host, bodyJson.toString());
-        String duckId = duckActions.validateResponseCreate(runner, host, bodyResponseJson.toString(), HttpStatus.OK);
-        duckActions.delete(runner, host, duckId);
-        duckActions.validateResponse(runner, host, "{" +
-                "  \"message\": \"Duck is deleted\"" +
-                "}", HttpStatus.OK);
+        duckActions.validateResponseCreate(runner, host, bodyResponseJson.toString(), HttpStatus.OK);
 
         //Меняем поле material на wood и отправляем запрос на создание утки
-        ((ObjectNode) bodyJson).put("material","wood");
-        ((ObjectNode) bodyResponseJson).put("material","wood");
+        ((ObjectNode) bodyJson).put("material", "wood");
+        ((ObjectNode) bodyResponseJson).put("material", "wood");
 
         duckActions.create(runner, host, bodyJson.toString());
-        duckId = duckActions.validateResponseCreate(runner, host, bodyResponseJson.toString(), HttpStatus.OK);
-        duckActions.delete(runner, host, duckId);
-        duckActions.validateResponse(runner, host, "{" +
-                "  \"message\": \"Duck is deleted\"" +
-                "}", HttpStatus.OK);
+        duckActions.validateResponseCreate(runner, host, bodyResponseJson.toString(), HttpStatus.OK);
+
     }
 
     @Test(description = "Метод удаления уточки")
     @CitrusTest
-    public void delete (@Optional @CitrusResource TestCaseRunner runner)
-    {
+    public void delete(@Optional @CitrusResource TestCaseRunner runner) {
         duckActions.create(runner, host, body);
         String duckId = duckActions.validateResponseCreate(runner, host, responseBody, HttpStatus.OK);
         duckActions.delete(runner, host, duckId);
@@ -83,56 +71,196 @@ public class DuckActionsTest extends TestNGCitrusSpringSupport {
                 "  \"message\": \"Duck is deleted\"" +
                 "}", HttpStatus.OK);
     }
-    @Test(description = "Метод изменения уточки")
+
+    @Test(description = "Метод изменения уточки (меняем цвет и высоту, цвет и звук")
     @CitrusTest
-    public void update (@Optional @CitrusResource TestCaseRunner runner) throws JsonProcessingException
-    {
-        ObjectMapper object  = new ObjectMapper();
+    public void update(@Optional @CitrusResource TestCaseRunner runner) throws JsonProcessingException {
+        String duckId = "1";
+        ObjectMapper object = new ObjectMapper();
         JsonNode bodyJson = object.readTree(body);
         JsonNode bodyResponseJson = object.readTree(responseBody);
+        try {
+            // Создаем утку
+            duckActions.create(runner, host, bodyJson.toString());
+            duckId = duckActions.validateResponseCreate(runner, host, bodyResponseJson.toString(), HttpStatus.OK);
+
+            //Меняем цвет и высоту уточки
+            ((ObjectNode) bodyJson).put("color", "red");
+            ((ObjectNode) bodyJson).put("height", 7);
+            duckActions.update(runner, host, bodyJson.get("color").toString(), bodyJson.get("height").toString(),
+                    duckId, bodyJson.get("material").toString(),
+                    bodyJson.get("sound").toString(), bodyJson.get("wingsState").toString());
+            duckActions.validateResponse(runner, host, "{" +
+                    "  \"message\": \"Duck with id = " + duckId + " is updated\"" + "}", HttpStatus.OK);
+        } finally {
+            //Удаляем утку
+            duckActions.delete(runner, host, duckId);
+            duckActions.validateResponse(runner, host, "{" +
+                    "  \"message\": \"Duck is deleted\"" +
+                    "}", HttpStatus.OK);
+        }
+
+        try {
+            // Создаем новую утку
+            bodyJson = object.readTree(body);
+            bodyResponseJson = object.readTree(responseBody);
+            duckActions.create(runner, host, bodyJson.toString());
+            duckId = duckActions.validateResponseCreate(runner, host, bodyResponseJson.toString(), HttpStatus.OK);
+
+            //Меняем цвет и звук уточки уточки (недопустимо менять звук утки, поэтому должна быть ошибка 400)
+            ((ObjectNode) bodyJson).put("color", "red");
+            ((ObjectNode) bodyJson).put("sound", "meow");
+            duckActions.update(runner, host, bodyJson.get("color").toString(), bodyJson.get("height").toString(),
+                    duckId, bodyJson.get("material").toString(),
+                    bodyJson.get("sound").toString(), bodyJson.get("wingsState").toString());
+            duckActions.validateResponse(runner, host, "{" +
+                    "  \"message\": \"Duck with id = " + duckId + " is not updated\"" + "}", HttpStatus.BAD_REQUEST);
+        } finally {
+            // Удаляем утку
+            duckActions.delete(runner, host, duckId);
+            duckActions.validateResponse(runner, host, "{" +
+                    "  \"message\": \"Duck is deleted\"" +
+                    "}", HttpStatus.OK);
+        }
+    }
+
+    @Test(description = "Метод получения характеристик уточки (Нечестный id material = rubber, четный materiai = wood")
+    @CitrusTest
+    public void properties(@Optional @CitrusResource TestCaseRunner runner) throws JsonProcessingException {
+        ObjectMapper object = new ObjectMapper();
+        JsonNode bodyJson = object.readTree(body);
+        JsonNode bodyResponseJson = object.readTree(responseBody);
+
+        // Оставляем поле material по умолчанию rubber и отправляем запрос на просмотр  свойств
         duckActions.create(runner, host, bodyJson.toString());
-        String duckId = duckActions.validateResponseCreate(runner, host, bodyResponseJson.toString(), HttpStatus.OK);
+        duckActions.validateResponseCreate(runner, host, responseBody, HttpStatus.OK);
+        ((ObjectNode) bodyResponseJson).remove("id");
+        duckActions.getProperties(runner, host, "1");
+        duckActions.validateResponse(runner, host, bodyResponseJson.toString(), HttpStatus.OK);
 
-        //Меняем цвет и высоту уточки
-        ((ObjectNode) bodyJson).put("color","red");
-        ((ObjectNode) bodyJson).put("height",7);
-        ((ObjectNode) bodyResponseJson).put("color","red");
-        ((ObjectNode) bodyResponseJson).put("height",7);
-        duckActions.update(runner, host,bodyJson. );
-        duckActions.delete(runner, host, duckId);
-        duckActions.validateResponse(runner, host, "{" +
-                "  \"message\": \"Duck is deleted\"" +
-                "}", HttpStatus.OK);
-        //Меняем цвет и //Меняем цвет и высоту уточки уточки
-        ((ObjectNode) bodyJson).put("color","red");
-        ((ObjectNode) bodyResponseJson).put("height",7);
-        duckActions.update();
+        //Меняем поле material на wood и отправляем запрос на просмотр свойств
+        ((ObjectNode) bodyJson).put("material", "wood");
+        ((ObjectNode) bodyResponseJson).put("material", "wood");
+        ((ObjectNode) bodyResponseJson).remove("id");
+        duckActions.update(runner, host, bodyJson.get("color").toString(), bodyJson.get("height").toString(),
+                "2", bodyJson.get("material").toString(),
+                bodyJson.get("sound").toString(), bodyJson.get("wingsState").toString());
+        duckActions.getProperties(runner, host, "1");
+        duckActions.validateResponse(runner, host, bodyResponseJson.toString(), HttpStatus.OK);
 
     }
-    @Test(description = "Метод получения характеристик уточки")
+
+    @Test(description = "Метод, позволяющий лететь или отказаться от полета уточке (крылья: ACTIVE, FIXED, UNDEFINED")
     @CitrusTest
-    public void properties (@Optional @CitrusResource TestCaseRunner runner)
-    {
+    public void fly(@Optional @CitrusResource TestCaseRunner runner) throws JsonProcessingException {
+        String duckId = "1";
+        ObjectMapper object = new ObjectMapper();
+        JsonNode bodyJson = object.readTree(body);
+        JsonNode bodyResponseJson = object.readTree(responseBody);
+        try {
+            // С активными крыльями
+            duckActions.create(runner, host, bodyJson.toString());
+            duckId = duckActions.validateResponseCreate(runner, host, bodyResponseJson.toString(), HttpStatus.OK);
+
+            duckActions.fly(runner, host, duckId);
+            duckActions.validateResponse(runner, host, "{" + "\"message\": \"I'm flying\"}", HttpStatus.OK);
+        } finally {
+            duckActions.delete(runner, host, duckId);
+            duckActions.validateResponse(runner, host, "{" +
+                    "  \"message\": \"Duck is deleted\"" +
+                    "}", HttpStatus.OK);
+        }
+        try {
+            // С связанными крыльями
+            bodyJson = object.readTree(body);
+            bodyResponseJson = object.readTree(responseBody);
+            duckActions.create(runner, host, bodyJson.toString());
+            ((ObjectNode) bodyJson).put("wingsState", "FIXED");
+            ((ObjectNode) bodyResponseJson).put("wingsState", "FIXED");
+            duckId = duckActions.validateResponseCreate(runner, host, bodyResponseJson.toString(), HttpStatus.OK);
+
+            duckActions.fly(runner, host, duckId);
+            duckActions.validateResponse(runner, host, "{" + "\"message\": \"I can't fly\"}", HttpStatus.OK);
+        } finally {
+            duckActions.fly(runner, host, duckId);
+            duckActions.delete(runner, host, duckId);
+            duckActions.validateResponse(runner, host, "{" +
+                    "  \"message\": \"Duck is deleted\"" +
+                    "}", HttpStatus.OK);
+        }
+        try {
+            // С неопределенными крыльями
+            bodyJson = object.readTree(body);
+            bodyResponseJson = object.readTree(responseBody);
+            duckActions.create(runner, host, bodyJson.toString());
+            ((ObjectNode) bodyJson).remove("wingsState");
+            ((ObjectNode) bodyResponseJson).put("wingsState", "UNDEFINED");
+            duckId = duckActions.validateResponseCreate(runner, host, bodyResponseJson.toString(), HttpStatus.OK);
+
+            duckActions.fly(runner, host, duckId);
+            duckActions.validateResponse(runner, host, "{" + "\"message\": \"Wings are not detected\"}",
+                    HttpStatus.INTERNAL_SERVER_ERROR);
+
+            duckActions.fly(runner, host, duckId);
+        } finally {
+            duckActions.delete(runner, host, duckId);
+            duckActions.validateResponse(runner, host, "{" +
+                    "  \"message\": \"Duck is deleted\"" +
+                    "}", HttpStatus.OK);
+        }
 
     }
-    @Test(description = "Метод, позволяющий лететь или отказаться от полета уточке")
+
+    @Test(description = "Метод, позволяющий плыть уточке (существующий id и несуществующий)")
     @CitrusTest
-    public void fly(@Optional @CitrusResource TestCaseRunner runner)
-    {
+    public void swim(@Optional @CitrusResource TestCaseRunner runner) {
+        String duckId = "";
+        try {
+            // Утка с существующим id
+            duckActions.create(runner, host, body);
+            duckId = duckActions.validateResponseCreate(runner, host, responseBody, HttpStatus.OK);
+            duckActions.swim(runner, host, duckId);
+            duckActions.validateResponse(runner, host, "{" + "\"message\": \"I'm swimming\"}", HttpStatus.OK);
+        } finally {
+            duckActions.delete(runner, host, duckId);
+            duckActions.validateResponse(runner, host, "{" +
+                    "  \"message\": \"Duck is deleted\"" +
+                    "}", HttpStatus.OK);
+        }
+        try {
+            // Утка с несуществующим id
+            duckActions.create(runner, host, body);
+            duckId = duckActions.validateResponseCreate(runner, host, responseBody, HttpStatus.OK);
+        } finally {
+            duckActions.delete(runner, host, duckId);
+            duckActions.validateResponse(runner, host, "{" +
+                    "  \"message\": \"Duck is deleted\"" +
+                    "}", HttpStatus.OK);
+            duckActions.swim(runner, host, duckId);
+            duckActions.validateResponse(runner, host, "{" + "\"message\": \"Paws are not found\"}", HttpStatus.NOT_FOUND);
+        }
 
     }
-    @Test(description = "Метод, позволяющий плыть уточке")
+
+    @Test(description = "Метод, позволяющий крякать уточке (четный и нечетный id)")
     @CitrusTest
-    public void swim(@Optional @CitrusResource TestCaseRunner runner)
-    {
+    public void quack(@Optional @CitrusResource TestCaseRunner runner) {
+        String duckId = "1";
+        for (int i = 0; i < 2; i++) {
+            try {
+                duckActions.create(runner, host, body);
+                duckId = duckActions.validateResponseCreate(runner, host, responseBody, HttpStatus.OK);
+                duckActions.quack(runner, host, duckId, "1", "1");
+                duckActions.validateResponse(runner, host, "{" +
+                        " \"sound\": \"quack\"" + "}", HttpStatus.OK);
+            } finally {
+                duckActions.delete(runner, host, duckId);
+                duckActions.validateResponse(runner, host, "{" +
+                        "  \"message\": \"Duck is deleted\"" +
+                        "}", HttpStatus.OK);
+            }
 
+        }
     }
-    @Test(description = "Метод, позволяющий крякать уточке")
-    @CitrusTest
-    public void quack(@Optional @CitrusResource TestCaseRunner runner)
-    {
-
-    }
-
 
 }
--- src/test/java/utils/DuckActionsUtils.java
+++ src/test/java/utils/DuckActionsUtils.java
@@ -1,19 +1,12 @@
 package utils;
-import org.springframework.test.context.TestContext;
-import org.testng.annotations.Optional;
-import com.consol.citrus.annotations.CitrusResource;
-import com.consol.citrus.annotations.CitrusTest;
+
 import com.consol.citrus.testng.spring.TestNGCitrusSpringSupport;
-import com.jayway.jsonpath.JsonPath;
 import org.springframework.http.HttpStatus;
-import org.testng.annotations.Test;
 import org.springframework.http.MediaType;
 import com.consol.citrus.TestCaseRunner;
 
-import static com.consol.citrus.actions.EchoAction.Builder.echo;
 import static com.consol.citrus.http.actions.HttpActionBuilder.http;
 import static com.consol.citrus.dsl.MessageSupport.MessageBodySupport.fromBody;
-import static com.consol.citrus.http.actions.HttpActionBuilder.http;
 
 public class DuckActionsUtils extends TestNGCitrusSpringSupport {
     public void create(TestCaseRunner runner, String host, String body) {
@@ -32,19 +25,21 @@ public class DuckActionsUtils extends TestNGCitrusSpringSupport {
                 .message()
                 .queryParam("id", id));
     }
+
     public void update(TestCaseRunner runner, String host, String color, String height, String id, String material,
-                       String sound , String wingsState) {
+                       String sound, String wingsState) {
         runner.$(http().client(host)
                 .send()
                 .put("/api/duck/update")
                 .message()
-                .queryParam("color", color)
+                .queryParam("color", color.replace("\"", ""))
                 .queryParam("height", height)
                 .queryParam("id", id)
-                .queryParam("material", material)
-                .queryParam("sound", sound)
-                .queryParam("wingsState", wingsState));
+                .queryParam("material", material.replace("\"", ""))
+                .queryParam("sound", sound.replace("\"", ""))
+                .queryParam("wingsState", wingsState.replace("\"", "")));
     }
+
     public void getProperties(TestCaseRunner runner, String host, String id) {
         runner.$(http().client(host)
                 .send()
@@ -72,12 +67,13 @@ public class DuckActionsUtils extends TestNGCitrusSpringSupport {
     public void quack(TestCaseRunner runner, String host, String id, String repetitionCount, String soundCount) {
         runner.$(http().client(host)
                 .send()
-                .get("/api/duck/action/swim")
+                .get("/api/duck/action/quack")
                 .message()
                 .queryParam("id", id)
                 .queryParam("repetitionCount", repetitionCount)
                 .queryParam("soundCount", soundCount));
     }
+
 
     public void validateResponse(TestCaseRunner runner, String host, String responseMessage, HttpStatus status) {
         runner.$(http().client(host)
@@ -87,6 +83,7 @@ public class DuckActionsUtils extends TestNGCitrusSpringSupport {
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .body(responseMessage));
     }
+
     public String validateResponseCreate(TestCaseRunner runner, String host, String responseMessage, HttpStatus status) {
         runner.$(http().client(host)
                 .receive()
@@ -97,5 +94,6 @@ public class DuckActionsUtils extends TestNGCitrusSpringSupport {
                 .body(responseMessage));
         return "${id}";
     }
+
 }
 
